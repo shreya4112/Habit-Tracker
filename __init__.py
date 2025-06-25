110d2cea6319e89a4e9104bd5730495b2b4714b4
SHREYA AGRAWAL <113137959+shreya4112@users.noreply.github.com>
2025-06-25 22:05:45 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,4 @@
-#this file is a initialization file of python where we will be announcing the project starting
+#this file is a initialization file of python
+
+#where we will be announcing the project starting
 
